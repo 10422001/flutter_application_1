d7465d1acb0255e80164622d2b435f27e94ae933
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-02-06 22:16:14 +0700

    generates new names all over the place

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,47 @@ class MyNameGenerator extends StatefulWidget {
 }
 
 class _MyName extends State<MyNameGenerator> {
+  final _myNames = <String>[];
+  final _hearted = <String>{};
   @override
   Widget build(BuildContext context) {
-    final myname = _wordGenerator.randomName();
-    return Text(myname);
+    return Scaffold(
+      appBar: AppBar(title: Text('Welcome to my random Name Generator')),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          if (i ~/ 2 >= _myNames.length) {
+            _myNames.addAll(_generateNames());
+          }
+          return _buildRow(_myNames[i ~/ 2]);
+        });
+  }
+
+  Iterable<String> _generateNames() {
+    List<String> myNames = [
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+      _wordGenerator.randomName(),
+    ];
+    return myNames;
+  }
+
+  Widget _buildRow(String myNam) {
+    return ListTile(title: Text(myNam));
   }
 }
---
+++ lib/voc100.dart
@@ -0,0 +1,12 @@
+List<String> names = [
+  '1. der / die / das (def. art.) the; (dem. pron.)  that, those; (rel. pron) who, that',
+  '2. und (conj.) and',
+  '3. sein (verb) to be; (aux./perfect tense)',
+  '4. in (prep.) in (variation: im in the)',
+  '5. ein (indef. art.) a, an; (pron.) one (of)',
+  '6. zu (prep.) to, at; (adv.) too',
+  '7. haben (verb) to have; (aux./perfect tense)',
+  '8. ich (pers. pron.) I',
+  '9. werden (verb) to become; (aux./future tense); ; (aux./passive voice)',
+  '10. sie (pers. pron.) she, her (acc. form of sie [sing.]); they, them (acc. form of sie [pl.]; Sie (pers. pron.) you (formal)'
+];
